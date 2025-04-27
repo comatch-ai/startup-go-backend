454ccebbd1742d18bc58d1cf403d06de730a504a
ToothlessOS <anthony.13661499167@gmail.com>
2025-04-27 10:29:44 +0800

    Fix: URLs

--- recommendations/urls.py
+++ recommendations/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import RecommendationView
 
 urlpatterns = [
-    path('recommendations/', RecommendationView.as_view(), name='recommendations'),
+    path('', RecommendationView.as_view(), name='recommendations'),
 ]
